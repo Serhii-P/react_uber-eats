080ba15d7c641568294a353fb1adf8e521bef5d4
Serhii-P <serhii.pyskun@gmail.com>
2020-05-04 16:01:38 +0300

    Updates

---
+++ precache-manifest.03285d79566b8f30efe560a4388f72a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a63337ed2f5cc4c79717eaaa764a6e5",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "6d8217f5d32777cfd0b5",
+    "url": "/react_uber-eats/static/css/main.5e300d67.chunk.css"
+  },
+  {
+    "revision": "02be31c11f9cfccf265b",
+    "url": "/react_uber-eats/static/js/2.f5ef26b5.chunk.js"
+  },
+  {
+    "revision": "6d8217f5d32777cfd0b5",
+    "url": "/react_uber-eats/static/js/main.f56158cc.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
